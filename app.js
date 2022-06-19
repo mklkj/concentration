27046e7e0dd964b79895e4510fb0aed96abe6ce7
Mikołaj Pich <m.pich@outlook.com>
2022-06-19 14:06:43 +0200

    Remove handlebars template engine

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const fs = require('fs');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -11,31 +9,24 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("view options", {layout: false});
+app.set('view engine', 'html');
+app.engine('html', function (path, opt, fn) {
+    fs.readFile(path, 'utf-8', function (error, str) {
+        if (error) return str;
+        return fn(null, str);
+    });
+});
 
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+  console.error(err);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sign up' });
+  res.render('index');
 });
 
 router.get('/success', function(req, res, next) {
-  res.render('success', { title: 'Congratulations!' });
+  res.render('success');
 });
 
 module.exports = router;
